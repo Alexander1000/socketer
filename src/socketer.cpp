cced99c46fa704f7574709152a4b6598c937fd0a
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-07 10:10:27 +0300

    add methods

--- src/socketer.cpp
+++ src/socketer.cpp
@@ -1,4 +1,6 @@
 #include <socketer.h>
+#include <string>
+#include <utility>
 
 namespace Socketer
 {
@@ -7,5 +9,21 @@ namespace Socketer
         // set defaults
         this->listen_host = "127.0.0.1";
         this->listen_port = 8890;
+    }
+
+    void Socketer::dispatch()
+    {
+        // do dispatch
+    }
+
+    void Socketer::listen(std::string host, int port)
+    {
+        this->listen_host = std::move(host);
+        this->listen_port = port;
+    }
+
+    void Socketer::addHandler(std::string uri, ServeHttpCallback callback)
+    {
+        // add handler there
     }
 }
