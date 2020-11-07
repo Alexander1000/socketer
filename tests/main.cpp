228ae405a5ec50159b306d0cb47251c95502d915
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-07 14:28:32 +0300

    add test asserts

--- tests/main.cpp
+++ tests/main.cpp
@@ -17,6 +17,21 @@ CppUnitTest::TestCase* testRequest_PostRequest_Positive()
 
     Socketer::Request r(raw_request);
 
+    // assert main header
+    CppUnitTest::assertEquals(t, "POST", r.method);
+    CppUnitTest::assertEquals(t, "/v1/save", r.uri);
+    CppUnitTest::assertEquals(t, "HTTP/1.1", r.protocol);
+
+    // assert headers
+    CppUnitTest::assertEquals(t, "127.0.0.1:50101", r.headers.at("Host"));
+    CppUnitTest::assertEquals(t, "curl/7.64.1", r.headers.at("User-Agent"));
+    CppUnitTest::assertEquals(t, "*/*", r.headers.at("Accept"));
+    CppUnitTest::assertEquals(t, "6", r.headers.at("Content-Length"));
+    CppUnitTest::assertEquals(t, "application/x-www-form-urlencoded", r.headers.at("Content-Type"));
+
+    // assert body
+    CppUnitTest::assertEquals(t, "test=1", r.raw_body);
+
     t->finish();
     return t;
 }
