c62b46a3ba15185c98d6afab49e9fba4cd389298
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-07 11:03:37 +0300

    initialize request

--- include/socketer/request.h
+++ include/socketer/request.h
@@ -1,12 +1,20 @@
 #ifndef SOCKETER_REQUEST_H
 #define SOCKETER_REQUEST_H
 
+#include <string>
+#include <map>
+
 namespace Socketer
 {
     class Request
     {
     public:
         Request(char* raw_request);
+        char* method;
+        char* uri;
+        char* protocol;
+        std::map<std::string, std::string> headers;
+        char* raw_body;
     };
 }
 
--- src/request.cpp
+++ src/request.cpp
@@ -1,1 +1,18 @@
 #include <socketer.h>
+#include <string>
+#include <map>
+
+namespace Socketer
+{
+    Request::Request(char* raw_request)
+    {
+        // set defaults
+        this->method = nullptr;
+        this->uri = nullptr;
+        this->protocol = nullptr;
+        this->headers = std::map<std::string, std::string>();
+        this->raw_body = nullptr;
+
+        // parse request
+    }
+}
