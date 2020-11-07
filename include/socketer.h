32ca86cfa160bf7a16a73849d51a95b4aaa3a671
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-07 09:57:05 +0300

    add cmake

--- include/socketer.h
+++ include/socketer.h
@@ -1,3 +1,17 @@
 #ifndef SOCKETER_H
 #define SOCKETER_H
+
+#include <string>
+
+namespace Socketer
+{
+    class Socketer
+    {
+    public:
+        Socketer();
+        void addHandler(std::string uri, ServeHttpCallback callback);
+        void dispatch();
+    };
+}
+
 #endif
---
+++ include/socketer/handler.h
@@ -0,0 +1,6 @@
+#ifndef SOCKETER_HEADER_H
+#define SOCKETER_HEADER_H
+
+typedef void() ServeHttpCallback;
+
+#endif
